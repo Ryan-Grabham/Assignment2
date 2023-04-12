101161d459acda36a3a2a12971c99206d0afc607
Ryan-Grabham <gra18001287@btc.ac.uk>
2023-04-12 09:25:27 +0100

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Ryan Grabham (Student) on 12/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
